1fffa48fde1859d64ef7e98c94fe32ba750fada8
Meresa Gidey <71267586+Meresa16@users.noreply.github.com>
2025-12-22 16:12:49 +0300

    Update CryptoDashboard.tsx

--- frontend_portfolio/src/components/CryptoDashboard.tsx
+++ frontend_portfolio/src/components/CryptoDashboard.tsx
@@ -215,7 +215,7 @@ const CryptoDashboard: React.FC = () => {
         </div>
 
         {view === 'OVERVIEW' && (
-          <div className="relative  w-full md:w-64">
+          <div className="relative w-full md:w-64">
             <Search className="absolute left-3 top-2.5 text-gray-500 h-4 w-4" />
             <input
               type="text"
@@ -223,7 +223,6 @@ const CryptoDashboard: React.FC = () => {
               value={searchTerm}
               onChange={(e) => setSearchTerm(e.target.value)}
               className="bg-[#15191f] border border-gray-700 rounded-full pl-10 pr-4 py-2 text-sm focus:outline-none focus:border-cyan-500 transition-colors w-full" 
-              {/* className="bg-[#15191f] border border-gray-700 rounded-full pl-10 pr-4 py-2 text-sm focus:outline-none focus:border-cyan-500 transition-colors w-64" */}
             />
           </div>
         )}
